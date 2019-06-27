cd22d529d9a44bdc01ffd59e575124a7c9887c50
Alejandro Juárez <alejandrojuarezck@gmail.com>
2019-06-26 23:55:29 -0600

    Main Controller

--- src/com/uca/capas/controller/MainController.java
+++ src/com/uca/capas/controller/MainController.java
@@ -1,55 +1,50 @@
 package com.uca.capas.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.uca.capas.domain.Account;
 import com.uca.capas.domain.User;
-
-import com.uca.capas.repositories.UserRepository;
 import com.uca.capas.service.AccountService;
 import com.uca.capas.service.UserService;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.springframework.data.domain.Sort;
-@Controller //manda a llamar a los metodos
+
+@Controller
 public class MainController {
-	
 	static Logger log = Logger.getLogger(MainController.class.getName());
 	
-
+	public static Account acc = null;
 	
-	
-
 	@Autowired
 	private AccountService accountServ;
 	
-	@RequestMapping("/")
+	@RequestMapping(value="/", method = RequestMethod.GET)
 	public ModelAndView initMain() {
-		
 		ModelAndView mav = new ModelAndView();
+		
 		mav.setViewName("login");
 		return mav;
 	}
 	
-	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public ModelAndView login(@RequestParam(value="username") String username,@RequestParam(value="password") String password) {
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ModelAndView login(@RequestParam(value="username") String username,@RequestParam(value="password") String password, final RedirectAttributes redirectAttributes) {
 		ModelAndView mav = new ModelAndView();
-		if(accountServ.findOneUser(username, password)) {
-			log.info("Entrando a funcion init-min" + log.getName());
-			mav.setViewName("main");
-		}else {
-			mav.setViewName("login");
+		Object accId = accountServ.findOneUser(username, password);
+		
+		if(accId != null) {
+			acc = this.accountServ.findOne((Integer) accId);
+			mav.setViewName("redirect:/dashboard-client");
+		} else {
+			redirectAttributes.addFlashAttribute("error", 0);
+			mav.setViewName("redirect:/login");
 		}
-			log.info("No se pudo realizar" + log.getName() +"u:::::::"+ username+ "p::::::"+password);
+		
 		return mav; 
 	}
 	
